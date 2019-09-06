f49cb30eef4a5f9565a5daa6991c9e5b8d894d96
sayoojbk <bsayooj2012@gmail.com>
2019-09-07 04:56:10 +0530

    done with Mongodb-data load :alien:

--- recommendation-model/utils/dataset.py
+++ recommendation-model/utils/dataset.py
@@ -1,5 +1,7 @@
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License.
+# ------------------------------------------------------------------------------------------------------
+# Licensed under MIT License
+# Written by sayooj_bk
+# -------------------------------------------------------------------------------------------------------
 
 import os
 import re
@@ -30,15 +32,19 @@ myclient = pymongo.MongoClient("mongodb://localhost:27017/")
 # Load the fcgl database which has to be loaded and
 """
 FCGL-DATABASE :
-COLLECTIONS : USER DATA
-              PRODUCT DATA
-              MERCHANT DATA
+COLLECTIONS : USER_DATA
+              PRODUCT_DATA
+              MERCHANT_DATA
+              PRODUCT_CATEGORY_DATA
+              MERCHANT_CATEGORY_DATA
+              MERCHANT_PRODUCT_DATA
+              USER_PURCHASES_DATA.
 
 
 """ 
 mydb = myclient["fcgl-database"]          
-training_collection = mydb["training_data"]
-# training_collection.insert(dict)   -- this will add the data to the colleciton.
+training_collection = mydb["training_data"]   
+# training_collection.insert(dict)   -- this is where we will add the data to be trained to the colleciton.
 
 ERROR_HEADER = "Header error. At least user and item id should be provided."
 WARNING_FCGL_HEADER = "The dataset has more than the required data so only few columns will be used #TODO select those few."
@@ -64,13 +70,12 @@ DEFAULT_HEADER = (
     DEFAULT_USER_BUDGET,
     DEFAULT_PURCHASED_COUNT,
     DEFAULT_NATIONALITY,
-    DEFAULT_ITEM_COL,
+    DEFAULT_ITEM_COL,                       # This is the data point to be evaluated or in a sense this has to be the result of the model.
     DEFAULT_ITEM_CATEGORY,
     DEFAULT_RETAIL_PRICE,
-    MERCHANT_PRODUCT_PRICE,
-    DISCOUNTED_PRICE,
-    MERCHANT_CATEGORY_ID,
-    PRICE_LEVEL
+    # MERCHANT_PRODUCT_PRICE,               # These three are the closest we have to find for the product which has to be recommended.
+    # DISCOUNTED_PRICE,                     # based on lowest cost and closest distance.
+    # PRICE_LEVEL
 )
 
 
@@ -116,116 +121,78 @@ def load_pandas_df(
         header = DEFAULT_HEADER
     elif len(header) < 2:
         raise ValueError(ERROR_HEADER)
-    elif len(header) > 4:
+    elif len(header) > 7:
         warnings.warn(WARNING_FCGL_HEADER)
         # header = header[:4]
 
-    fcgl_column = header[1]
+    #  fcgl_column = header[1]
+    
     # collection_name : Collection name that is used to train the vowpal wabbit model.
     # collection_name : Since this is what helps in recommnding we will push in the intial user data we get from the user while he/she
     # signups for the application.
+
+    # data provided by the user.
+    user_data = mydb["user_data"]
+    user_purchase_data = mydb["user_purchase_data"]
+    product_data = mydb["product_data"]
+    product_category_data = mydb["product_category_data"]
+    
+    
+    for user in user_data :
+        new_data = user
+        # This new data = {"_id":mongodb id provided,  user_id : "" , nationality:"" , average_spent: "" }
+        try: 
+            del new_data['_id']
+        except KeyError: 
+            pass
+        
+        # This get the purhcase query data which has item id and purchase count
+        purchase_query = dict()
+        purchase_query['user_id'] = new_data['user_id']
+       
+        user_purchase = user_purchase_data.find(purchase_query)
+        
+        for key in ['_id' , 'added_on' , 'last_updated' , 'user_id'] :
+            try: 
+                del user_purchase[key]
+            except KeyError: 
+                pass
+        
+        # user_purchase = { product_id , purchase_count}
+
+        product_query = dict()
+        product_query['product_id'] = user_purchase['product_id']
+
+        product_category = product_category_data.find(product_query)
+        product_price    = product_data.find(product_query)
+
+        for key in ['_id' , 'added_on' , 'last_updated', 'product_id'] :
+            try: 
+                del product_category[key]
+            except KeyError: 
+                pass
+
+        for key in ['_id' , 'added_on' , 'last_updated' , 'name' , 'categories', 'product_id'] :
+            try: 
+                del product_price[key]
+            except KeyError: 
+                pass
+        
+        # prodcut-cat = {category}
+        # product price ={price}
+    
+
+        new_data.update(user_purchase)
+        new_data.update(product_category)
+        new_data.update(product_price)
+
+        # new_data = {"user_id" , "nationality" , "item_id" , "purchase_count" , "budget" , "item_cat" , "item_retail_price."}
+        training_collection.insert_one(new_data) 
 
 
     df = DataFrame( list(mydb.training_data.find({})) )
     
     return df
-
-
-def load_item_df(
-    movie_col=DEFAULT_ITEM_COL,
-    title_col=None,
-    genres_col=None,
-    year_col=None):
-    """Loads Movie info.
-
-    Args:
-        movie_col (str): Movie id column name.
-        title_col (str): Movie title column name. If None, the column will not be loaded.
-        genres_col (str): Genres column name. Genres are '|' separated string.
-            If None, the column will not be loaded.
-        year_col (str): Movie release year column name. If None, the column will not be loaded.
-
-    Returns:
-        pd.DataFrame: Movie information data, such as title, genres, and release year.
-    """
-    size = size.lower()
-    if size not in DATA_FORMAT:
-        raise ValueError(ERROR_MOVIE_LENS_SIZE)
-
-    with download_path(local_cache_path) as path:
-        filepath = os.path.join(path, "ml-{}.zip".format(size)) 
-        _, item_datapath = _maybe_download_and_extract(size, filepath)
-        item_df = _load_item_df(
-            size, item_datapath, movie_col, title_col, genres_col, year_col
-        )
-
-    return item_df
-
-
-def _load_item_df(size, item_datapath, movie_col, title_col, genres_col, year_col):
-    """Loads Movie info"""
-    if title_col is None and genres_col is None and year_col is None:
-        return None
-
-    item_header = [movie_col]
-    usecols = [0]
-
-    # Year is parsed from title
-    if title_col is not None or year_col is not None:
-        item_header.append("title_year")
-        usecols.append(1)
-
-    genres_header_100k = None
-    if genres_col is not None:
-        # 100k data's movie genres are encoded as a binary array (the last 19 fields)
-        # For details, see http://files.grouplens.org/datasets/movielens/ml-100k-README.txt
-        if size == "100k":
-            genres_header_100k = [*(str(i) for i in range(19))]
-            item_header.extend(genres_header_100k)
-            usecols.extend([*range(5, 24)])  # genres columns
-        else:
-            item_header.append(genres_col)
-            usecols.append(2)  # genres column
-
-    item_df = pd.read_csv(
-        item_datapath,
-        sep=DATA_FORMAT[size].item_separator,
-        engine="python",
-        names=item_header,
-        usecols=usecols,
-        header=0 if DATA_FORMAT[size].item_has_header else None,
-        encoding="ISO-8859-1",
-    )
-
-    # Convert 100k data's format: '0|0|1|...' to 'Action|Romance|..."
-    if genres_header_100k is not None:
-        item_df[genres_col] = item_df[genres_header_100k].values.tolist()
-        item_df[genres_col] = item_df[genres_col].map(
-            lambda l: "|".join([GENRES[i] for i, v in enumerate(l) if v == 1])
-        )
-
-        item_df.drop(genres_header_100k, axis=1, inplace=True)
-
-    # Parse year from movie title. Note, MovieLens title format is "title (year)"
-    # Note, there are very few records that are missing the year info.
-    if year_col is not None:
-
-        def parse_year(t):
-            parsed = re.split("[()]", t)
-            if len(parsed) > 2 and parsed[-2].isdecimal():
-                return parsed[-2]
-            else:
-                return None
-
-        item_df[year_col] = item_df["title_year"].map(parse_year)
-        if title_col is None:
-            item_df.drop("title_year", axis=1, inplace=True)
-
-    if title_col is not None:
-        item_df.rename(columns={"title_year": title_col}, inplace=True)
-
-    return item_df
-
 
 def load_spark_df(
     spark):
---
+++ some_scripts.py
@@ -0,0 +1,16 @@
+
+    # for post in posts.find():   bulk quererying.
+  
+    # myquery = { "address": "Valley 345" }  update collection.
+    # newvalues = { "$set": { "address": "Canyon 123" } }
+    # mycol.update_one(myquery, newvalues)
+
+    # import pymongo
+    # myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    # mydb = myclient["mydatabase"]
+    # mycol = mydb["customers"]
+    # myquery = { "address": "Park Lane 38" }
+    # mydoc = mycol.find(myquery)
+    # for x in mydoc:
+    # print(x)
+    # {'_id': 11, 'name': 'Ben', 'address': 'Park Lane 38'}
